bf4906af9985a78cb62d659cd0226fe4452a7859
José Alberto Cruz Martínez <albertocruz8133@proton.me>
2025-09-02 18:20:00 -0600

    fix: agrega variables par BUN, HOMEBRE y ejecucion startship junto al archivo de configuración de startship.toml

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,8 +4,22 @@ end
 
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-if status is-interactive
-    eval (zellij setup --generate-auto-start fish | string collect)
+#if status is-interactive
+#   eval (zellij setup --generate-auto-start fish | string collect)
+#end
+
+if not set -q ZELLIJ
+    zellij
 end
 
 set -gx PROJECT_PATHS ~/workspace ~/.config
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+starship init fish | source
+fastfetch
+
+# opencode
+fish_add_path /home/tito/.opencode/bin
